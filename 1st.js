0b26820cff3249c7af15285f7e9e002013b9e4f6
Ransilu <sankalparansilu@gmail.com>
2018-11-30 01:09:24 +0530

    some init msg

---
+++ 1st.js
@@ -0,0 +1,32 @@
+let name = 'ransilu';
+// console.log(name);
+
+ 
+
+ let n = n => n;
+
+ //console.log(n(name));
+
+ //let display= (x,y)=>x+y;
+
+//display(5,7)
+ //nconsole.log(display(3,5));
+
+ let display = (x,y)=>x*y;
+
+ console.log(display(3,4));
+
+let dis = (a,b)=>a/b;
+
+console.log(dis (8,2));
+
+let disp = (f,g)=>(f-g);
+
+console.log(disp (6,3));
+
+let displ = (k,l)=>(k*l);
+
+console.log(displ (2,3));
+
+
+
---
+++ 2st.js
@@ -0,0 +1,29 @@
+// //object oriented programing
+
+
+class CokieCutter{
+
+    constructor()
+    {
+      console.log('first i start')
+    }
+
+    mix(x,y)
+    {
+      if(x === 'sugar' && y==='flower')
+      {
+         return 'mixture is rady'      
+          
+      }
+      
+       return 'what  do you want please define !';      
+    }
+}
+
+
+// cookie =  new CokieCutter();
+// cookie2 = new CokieCutter();
+// console.log(cookie2.mix('h','u'));
+
+
+
